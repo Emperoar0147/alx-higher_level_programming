632772db0ba3b890b86ea75c2118e0b7eac5a72e
“Sylvanus <sylvanusuzor@gmail.com>
2024-01-18 05:11:44 -0800

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-# Import the add function from add_0.py
-from add_0 import add
-
-# Assign the values to variables a and b
-a = 1
-b = 2
-
-# Check if the script is the main program and not an imported module
 if __name__ == "__main__":
-    # Call the add function with a and b and print the result
-    result = add(a, b)
+    from add_0 import add
+    a = 1
+    b = 2
     print(f"{a} + {b} = {result}\n")
+    result = add(a, b)
