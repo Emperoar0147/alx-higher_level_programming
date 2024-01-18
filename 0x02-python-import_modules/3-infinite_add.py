01f1d97da18e1067b27bf584f81e98086e2cb088
“Sylvanus <sylvanusuzor@gmail.com>
2024-01-18 08:36:24 -0800

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-def main():
-    argv = sys.argv
-    num_args = len(argv) - 1
-    if num_args == 0:
-        print("No arguments provided.")
-    else:
-        total = sum(int(arg) for arg in argv[1:])
-        print(f"Result of the addition of all arguments: {total}")
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
