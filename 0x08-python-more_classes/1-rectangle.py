cecb316a9b29431349eea3b08846219fe3301ce3
“Sylvanus <sylvanusuzor@gmail.com>
2024-02-13 23:47:22 -0800

    1. Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,42 @@
 #!/usr/bin/python3
-"""Defines a UniqueRectangle class."""
+"""Defines a Rectangle class."""
 
 
-class UniqueRectangle:
-    """Represent a unique rectangle."""
+class Rectangle:
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new UniqueRectangle.
+        """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the new unique rectangle.
-            height (int): The height of the new unique rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        self.set_width(width)
-        self.set_height(height)
+        self.width = width
+        self.height = height
 
-    def set_width(self, value):
-        """Set the width of the rectangle."""
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    def get_width(self):
-        """Get the width of the rectangle."""
-        return self.__width
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
 
-    def set_height(self, value):
-        """Set the height of the rectangle."""
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    def get_height(self):
-        """Get the height of the rectangle."""
-        return self.__height
