8f49e74bc6681981fc96c78c75bfb83b16eb9a49
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-13 07:39:56 -0700

    updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+filename=$2
+if ! [ -f "$filename" ]; then
+    echo "File $filename not found"
+    exit 1
+fi
 json=$(cat "$filename")
 curl -s -X POST -H "Content-Type: application/json" -d "$json" "$1"
