65702c47d3a722bec6371337bd76dbfc17a3ae75
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-13 07:14:06 -0700

    updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument
-curl -s -X POST -H "Content-Type: application/json" -d "$json" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" | grep "Valid JSON" && echo "Valid JSON" || echo "Not a valid JSON"
