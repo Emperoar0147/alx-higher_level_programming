c50f59ffb0b5bb199394172e78eb7ace2c6c56b1
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-13 07:35:16 -0700

    updated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" | grep -q "Not a valid JSON" && echo "Not a valid JSON" || echo "Valid JSON"
+json=$(cat "$filename")
+curl -s -X POST -H "Content-Type: application/json" -d "$json" "$1"
