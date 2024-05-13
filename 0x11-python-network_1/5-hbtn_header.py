cd3dbf9966d55086aea2a4de60f9a1ad24892f31
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-13 08:58:57 -0700

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,6 @@ import sys
 
 url = sys.argv[1]
 
-response = requests.get(url)
-x_request_id = response.headers.get('X-Request-Id')
-
-print(x_request_id)
+resp = requests.get(url)
+x_request_id = resp.headers.get('X-Request-Id')
+print(resp.headers.get("X-Request-Id"))
