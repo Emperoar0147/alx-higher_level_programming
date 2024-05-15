fe7ca9c2e43583dee11d91bca1aaffa85261e5f9
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-14 21:36:55 -0700

    correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays the body
-of the response (decoded in utf-8).
+a Script that takes in a URL, sends a request to the URL and displays the body
+of the response.
 
-Handles urllib.error.HTTPError exceptions and prints: Error code: followed by the HTTP status code
+it Handles urllib.error.HTTPError exceptions and prints: Error code: followed by the HTTP status code
 """
 
 import urllib.request
