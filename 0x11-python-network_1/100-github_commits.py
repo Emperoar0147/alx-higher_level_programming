c962943e29f442c87d2ceb4ee93646882196cb6e
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-14 21:21:50 -0700

    correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,16 +11,15 @@ repo_name = sys.argv[1]
 owner_name = sys.argv[2]
 
 if __name__ == "__main__":
-url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-params = {'per_page': 10}
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-response = requests.get(url, params=params)
-
-if response.status_code == 200:
-    commits = response.json()
-    for commit in commits:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{sha}: {author_name}")
-else:
-    print("Error:", response.status_code)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
