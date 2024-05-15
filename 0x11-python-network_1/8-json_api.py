4d9e926378ad0ca1fda87998db4a4e548caacc6c
“Sylvanus <sylvanusuzor@gmail.com>
2024-05-14 21:11:08 -0700

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,21 +7,16 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 import sys
 
-if len(sys.argv) == 1:
-    q = ""
-else:
-    q = sys.argv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-url = "http://0.0.0.0:5000/search_user"
-data = {'q': q}
-
-response = requests.post(url, data=data)
-
-try:
-    json_response = response.json()
-    if json_response:
-        print("[{}] {}".format(json_response['id'], json_response['name']))
-    else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
